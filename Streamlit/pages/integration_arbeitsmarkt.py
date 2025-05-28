33322d4c764a2d7fc3015805cbe637eb9a0a73e0
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-28 10:52:39 +0200

    Add files via upload

---
+++ Streamlit/pages/integration_arbeitsmarkt.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show():
+    st.title("💼 Integration: Arbeitsmarkt")
+    st.markdown("""
+    - Beschäftigungsquote im Vergleich zur Gesamtbevölkerung  
+    - Typische Berufsfelder und Branchen  
+    - Einflussfaktoren: Herkunftsregion, Aufenthaltsdauer, Bildungsniveau  
+    """)
---
+++ Streamlit/pages/start.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def show():
+    st.title("Migration und Integration in Deutschland")
+    st.header("Zielsetzung:")
+    st.markdown("""
+    Dieses Projekt untersucht die **Migration und Integration in Deutschland** anhand öffentlich zugänglicher Daten.
+
+    Ziel ist es, ein differenziertes Bild über:
+    - die **demografische Entwicklung**
+    - die **Herkunft und regionale Verteilung**
+    - sowie den **Integrationsstand** in Bereichen wie Arbeitsmarkt, Sprache, Bildung und Einbürgerung  
+    zu gewinnen.
+
+    Es sollen Trends, Herausforderungen und Fortschritte sichtbar gemacht werden.
+    """)
