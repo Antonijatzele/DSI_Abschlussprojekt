9089bc74ec41b79d04230f527bf880e7e26d841e
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-30 15:28:59 +0200

    Update integration_bildung.py

--- Streamlit/pages/integration_bildung.py
+++ Streamlit/pages/integration_bildung.py
@@ -4,6 +4,19 @@ from PIL import Image
 def show():
     st.title("🎓 Integration: Bildung")
     st.image("https://raw.githubusercontent.com/Antonijatzele/DSI_Abschlussprojekt/main/images/Bildung_Tableau_Karte.png")
+
+
+    # Tableau Dashboard URL
+    tableau_url = "https://public.tableau.com/views/DeinDashboardName/DeinSheetName"
     
+    # Iframe mit Streamlit anzeigen
+    st.components.v1.html(
+        f"""
+        <iframe src="{tableau_url}" width="1000" height="800" frameborder="0"></iframe>
+        """,
+        height=800,
+    )
+
+
 # Funktion aufrufen
 show()
