0cb51b9fa7103c599c50bf2fc4671a2c09d55293
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-30 14:41:42 +0200

    Update integration_bildung.py

--- Streamlit/pages/integration_bildung.py
+++ Streamlit/pages/integration_bildung.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 
 def show():
@@ -8,12 +9,37 @@ def show():
     - Unterschiede zwischen den Bundesländern  
     """)
 
-    # URL deines Tableau-Dashboards (öffentliche Ansicht)
-    tableau_url = "https://public.tableau.com/views/Bildungsintegration/Blatt1"
+    html_code = """
+    <div class='tableauPlaceholder' id='viz1748608584589' style='position: relative'>
+      <noscript>
+        <a href='#'>
+          <img alt='Blatt 1 ' src='https://public.tableau.com/static/images/Bi/Bildungsintegration/Blatt1/1_rss.png' style='border: none' />
+        </a>
+      </noscript>
+      <object class='tableauViz'  style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Bildungsintegration/Blatt1' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Bi/Bildungsintegration/Blatt1/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='de-DE' />
+      </object>
+    </div>                
+    <script type='text/javascript'>
+      var divElement = document.getElementById('viz1748608584589');
+      var vizElement = divElement.getElementsByTagName('object')[0];
+      vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+      var scriptElement = document.createElement('script');
+      scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+      vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    </script>
+    """
     
-    # Einbetten via iframe
-    iframe_code = f'''
-    <iframe src="{tableau_url}" width="900" height="700" frameborder="0"></iframe>
-    '''
-    
-    st.markdown(iframe_code, unsafe_allow_html=True)
+    components.html(html_code, height=800, scrolling=True)
