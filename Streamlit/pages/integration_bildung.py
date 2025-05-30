86a3d5743204896ac914fdea65d3f6019337bf6a
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-30 14:27:08 +0200

    Update integration_bildung.py

--- Streamlit/pages/integration_bildung.py
+++ Streamlit/pages/integration_bildung.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 def show():
     st.title("🎓 Integration: Bildung")
@@ -6,3 +7,11 @@ def show():
     - Schulerfolg und Bildungsabschlüsse von Menschen mit Migrationshintergrund  
     - Unterschiede zwischen den Bundesländern  
     """)
+
+    # Tableau Public Dashboard anzeigen
+    tableau_html = """
+    <iframe src="https://public.tableau.com/views/Bildungsintegration/Blatt1?:language=de&:display_count=yes&:toolbar=yes"
+    width="1000" height="800" frameborder="0"></iframe>
+    """
+    
+    components.html(tableau_html, height=800)
