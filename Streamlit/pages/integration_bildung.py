56cd5d04d4f807dccf640bc87d02cfe22fde0c9b
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-30 15:18:58 +0200

    Update integration_bildung.py

--- Streamlit/pages/integration_bildung.py
+++ Streamlit/pages/integration_bildung.py
@@ -3,23 +3,7 @@ from PIL import Image
 
 def show():
     st.title("🎓 Integration: Bildung")
-    st.markdown("""
-    - Schulerfolg und Bildungsabschlüsse von Menschen mit Migrationshintergrund  
-    - Unterschiede zwischen den Bundesländern  
-    """)
-
-     # Bild über direkte URL laden
-    image_url = "https://raw.githubusercontent.com/Antonijatzele/DSI_Abschlussprojekt/main/images/Bildung_Tableau_Karte.png"
-    st.image(image_url, caption="Anteil ausländischer Schüler:innen", use_column_width=True)
-    st.title("Test")
-    st.markdown(
-    """
-    <a href="https://public.tableau.com/views/Bildungsintegration/Blatt1" target="_blank">
-        <button style="padding:10px 20px; font-size:16px;">📊 Zum Tableau Dashboard</button>
-    </a>
-    """,
-    unsafe_allow_html=True
-    )
-
+    st.image("https://raw.githubusercontent.com/Antonijatzele/DSI_Abschlussprojekt/main/images/Bildung_Tableau_Karte.png")
+    
 # Funktion aufrufen
 show()
