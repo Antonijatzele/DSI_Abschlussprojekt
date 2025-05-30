12b646ac0a0bf741b26cd0052ecc665bef576dc6
Antonijatzele <antonija.tzelepidis@web.de>
2025-05-30 16:55:47 +0200

    Update integration_bildung.py

--- Streamlit/modules/integration_bildung.py
+++ Streamlit/modules/integration_bildung.py
@@ -1,14 +1,7 @@
 import streamlit as st
-from PIL import Image
 
 def show():
     st.title("🎓 Integration: Bildung")
 
 
-import streamlit as st
-tableau_url = "https://public.tableau.com/app/profile/antonija.tzelepidis/viz/Bildungsintegration/Blatt1"
-st.markdown(f"[Zum Tableau Dashboard]({tableau_url})")
 
-
-# Funktion aufrufen
-show()
