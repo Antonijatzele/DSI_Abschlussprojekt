3d949585bb445a8251f145ab50c428b618a7d20a
Irrux <clemens.zaengel@web.de>
2025-06-01 21:53:41 +0200

    Einbau der Änderung in app.py

--- Streamlit/modules/migration_alter.py
+++ Streamlit/modules/migration_alter.py
@@ -4,104 +4,104 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import os
 
+def show():
+    altersreihenfolge = list(range(85))
 
-altersreihenfolge = list(range(85))
+    def load_and_prepare_data(file_path, filter):
+        df = pd.read_csv(file_path)
 
-def load_and_prepare_data(file_path, filter):
-    df = pd.read_csv(file_path)
+        if filter:
+            gruppen = df["Ländergruppierungen"].unique()
+            selected_group = st.selectbox(f"Wähle eine Ländergruppierung (Ausländer)", sorted(gruppen, key= lambda x: "$" if x=="Insgesamt" else x))
+            df = df[df["Ländergruppierungen"] == selected_group]
+        else:
+            selected_group = "Deutsche"
+            df = df
+        
+        # Alter nur bis 84 anzeigen
+        df = df[df["ALT"].isin(altersreihenfolge)]
 
-    if filter:
-        gruppen = df["Ländergruppierungen"].unique()
-        selected_group = st.selectbox(f"Wähle eine Ländergruppierung (Ausländer)", sorted(gruppen, key= lambda x: "$" if x=="Insgesamt" else x))
-        df = df[df["Ländergruppierungen"] == selected_group]
-    else:
-        selected_group = "Deutsche"
-        df = df
-    
-    # Alter nur bis 84 anzeigen
-    df = df[df["ALT"].isin(altersreihenfolge)]
+        #Männer und Frauen trennen
+        df_men = df[df["GES"] == "GESM"].set_index("ALT")["Value"].reindex(altersreihenfolge).fillna(0)
+        df_women = df[df["GES"] == "GESW"].set_index("ALT")["Value"].reindex(altersreihenfolge).fillna(0)
 
-    #Männer und Frauen trennen
-    df_men = df[df["GES"] == "GESM"].set_index("ALT")["Value"].reindex(altersreihenfolge).fillna(0)
-    df_women = df[df["GES"] == "GESW"].set_index("ALT")["Value"].reindex(altersreihenfolge).fillna(0)
+        return df_men, df_women, selected_group
 
-    return df_men, df_women, selected_group
-
-# Titel
-st.title("Alterverteilung im Vergleich")
-
-
-# Ausländer
-file_path_ausl = os.path.join("data", "migration", "alterspyramide.csv")
-df_men_ausl, df_women_ausl, selected_group_ausl = load_and_prepare_data(file_path_ausl, True )
-
-# Deutsche
-file_path_de = os.path.join("data", "migration", "alterspyramide_de.csv")
-df_men_de, df_women_de, selected_group_de = load_and_prepare_data(file_path_de, False)
+    # Titel
+    st.title("Alterverteilung im Vergleich")
 
 
-fig = make_subplots(
-    rows=1, cols=2,
-    shared_yaxes=True,
-    horizontal_spacing=0.05,
-    subplot_titles=(f"Ausländer ({selected_group_ausl})", "Deutsche")
-)
+    # Ausländer
+    file_path_ausl = os.path.join("data", "migration", "alterspyramide.csv")
+    df_men_ausl, df_women_ausl, selected_group_ausl = load_and_prepare_data(file_path_ausl, True )
 
-# Ausländer
-fig.add_trace(go.Bar(
-    y=altersreihenfolge,
-    x=-df_men_ausl.values,
-    name='Männer',
-    orientation='h',
-    marker=dict(color='steelblue'),
-    showlegend=False
-), row=1, col=1)
+    # Deutsche
+    file_path_de = os.path.join("data", "migration", "alterspyramide_de.csv")
+    df_men_de, df_women_de, selected_group_de = load_and_prepare_data(file_path_de, False)
 
-fig.add_trace(go.Bar(
-    y=altersreihenfolge,
-    x=df_women_ausl.values,
-    name='Frauen',
-    orientation='h',
-    marker=dict(color='lightcoral'),
-    showlegend=False
-), row=1, col=1)
 
-# Deutsche
-fig.add_trace(go.Bar(
-    y=altersreihenfolge,
-    x=-df_men_de.values,
-    name='Männer',
-    orientation='h',
-    marker=dict(color='steelblue'),
-    showlegend=False
-), row=1, col=2)
+    fig = make_subplots(
+        rows=1, cols=2,
+        shared_yaxes=True,
+        horizontal_spacing=0.05,
+        subplot_titles=(f"Ausländer ({selected_group_ausl})", "Deutsche")
+    )
 
-fig.add_trace(go.Bar(
-    y=altersreihenfolge,
-    x=df_women_de.values,
-    name='Frauen',
-    orientation='h',
-    marker=dict(color='lightcoral'),
-    showlegend=False
-), row=1, col=2)
+    # Ausländer
+    fig.add_trace(go.Bar(
+        y=altersreihenfolge,
+        x=-df_men_ausl.values,
+        name='Männer',
+        orientation='h',
+        marker=dict(color='steelblue'),
+        showlegend=False
+    ), row=1, col=1)
 
-# Layout
-fig.update_layout(
-    height=800,
-    barmode='overlay',
-    bargap=0.1,
-    xaxis=dict(
-        title='',
-        tickvals=[],
-        ticktext=[]
-    ),
-    xaxis2=dict(
-        title='',
-        tickvals=[],
-        ticktext=[]
-    ),
-    yaxis=dict(title='Alter'),
-    title="Alterspyramiden: Ausländer vs. Deutsche"
-)
+    fig.add_trace(go.Bar(
+        y=altersreihenfolge,
+        x=df_women_ausl.values,
+        name='Frauen',
+        orientation='h',
+        marker=dict(color='lightcoral'),
+        showlegend=False
+    ), row=1, col=1)
 
-st.plotly_chart(fig, use_container_width=True)
+    # Deutsche
+    fig.add_trace(go.Bar(
+        y=altersreihenfolge,
+        x=-df_men_de.values,
+        name='Männer',
+        orientation='h',
+        marker=dict(color='steelblue'),
+        showlegend=False
+    ), row=1, col=2)
+
+    fig.add_trace(go.Bar(
+        y=altersreihenfolge,
+        x=df_women_de.values,
+        name='Frauen',
+        orientation='h',
+        marker=dict(color='lightcoral'),
+        showlegend=False
+    ), row=1, col=2)
+
+    # Layout
+    fig.update_layout(
+        height=800,
+        barmode='overlay',
+        bargap=0.1,
+        xaxis=dict(
+            title='',
+            tickvals=[],
+            ticktext=[]
+        ),
+        xaxis2=dict(
+            title='',
+            tickvals=[],
+            ticktext=[]
+        ),
+        yaxis=dict(title='Alter'),
+        title="Alterspyramiden: Ausländer vs. Deutsche"
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
--- Streamlit/modules/migration_kreise.py
+++ Streamlit/modules/migration_kreise.py
@@ -4,26 +4,27 @@ import branca.colormap as cm
 import geopandas as gpd
 from pathlib import Path
 from shapely.geometry import Point
-import folium
 from streamlit_folium import st_folium
 
-st.header('Kartentest')
 
 # Geodaten laden
 @st.cache_data
 def lade_geodaten():
-    gdf = gpd.read_file(Path(__file__).parent / "data/kreise_mit_daten.geojson")
+    gdf = gpd.read_file(Path("data/migration/kreise_mit_daten.geojson"))
     gdf = gdf.to_crs("EPSG:4326")
 
     # Farbskala erstellen
-    colormap = cm.linear.YlGnBu_09.scale(7, gdf['Anteil_Zahl'].max())
-    colormap.caption = 'Wert pro Region'  # Beschriftung der Legende
+    min_value = gdf['Anteil_Zahl'].min()
+    max_value = gdf['Anteil_Zahl'].max()
+    colormap = cm.linear.YlGnBu_09.scale(min_value, max_value)
+    colormap.caption = 'Ausländeranteil'  # Beschriftung der Legende
 
     # Farben auf Basis der Skala zuweisen
     gdf['color'] = gdf['Anteil_Zahl'].apply(colormap)
+    gdf['geometry'] = gdf['geometry'].simplify(tolerance=0.0001, preserve_topology=True)
 
-    return gdf.to_json()
-geojson_str = lade_geodaten()
+    return gdf.to_json(), colormap
+
 
 
 # Karte erstellen
@@ -41,29 +42,15 @@ def erstelle_karte(geojson_str):
         tooltip=folium.GeoJsonTooltip(fields=['Kreise', 'Gesamt', 'Anteil', 'Top Herk.-Länder:' , '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '10.'],)
     ).add_to(m)
     return m
-m = erstelle_karte(geojson_str)
 
-# Farbskala (Legende) zur Karte hinzufügen
-#colormap.add_to(m)
 
-# Karte anzeigen
-map_data = st_folium(m, width=700, height=700)
+def show():
+    st.header('Kreise')
+    geojson_str, colormap = lade_geodaten()
+    m = erstelle_karte(geojson_str)
+    
+    # Farbskala (Legende) zur Karte hinzufügen
+    colormap.add_to(m)
 
-'''
-# Wenn geklickt wurde
-if map_data and map_data.get("last_clicked"):
-    lat = map_data["last_clicked"]["lat"]
-    lon = map_data["last_clicked"]["lng"]
-    st.write(f"{lat} , {lon}")
-
-    point = Point(lon, lat)
-
-    # Zeile finden, die den Punkt enthält
-    clicked_row = gdf[gdf.contains(point)]
-
-    if not clicked_row.empty:
-        st.write("Gefundenes Land:")
-        st.dataframe(clicked_row[["Kreise"]])
-    else:
-        st.write("Kein Land gefunden.")
-'''
+    # Karte anzeigen
+    map_data = st_folium(m, width=700, height=700)
