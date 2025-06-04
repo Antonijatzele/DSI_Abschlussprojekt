95e6ddd0ec3fd4727f30614d1cf214f48d789d8e
Irrux <clemens.zaengel@web.de>
2025-06-04 17:03:49 +0200

    Überschriften wieder hinzugefügt

--- Streamlit/modules/migration_migration.py
+++ Streamlit/modules/migration_migration.py
@@ -2,6 +2,8 @@ import streamlit as st
 from modules.plots import simple_timeline
 
 def show():
+    st.header("Migration - Wanderungsbewegungen")
+
     tab1, tab2, tab3 = st.tabs(["Übersicht", "Herkunftsländer", "Vergleich Deutsche"])
 
     with tab1:
@@ -18,6 +20,8 @@ def show():
         st.subheader("Migration deutscher Staatbürger")
         default_groups = None
         simple_timeline("wander_gesamt_de.csv", "Art", default_groups)
+
+    st.markdown("Quelle: [Destatis - Wanderungsstatistik](https://www-genesis.destatis.de/datenbank/online/statistic/12711/details)")
 
 
 if __name__ == "__main__":
