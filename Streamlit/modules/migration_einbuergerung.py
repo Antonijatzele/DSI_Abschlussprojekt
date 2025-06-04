326dbea5dbf4b58afcbc562a2be929a97040cbfd
Irrux <clemens.zaengel@web.de>
2025-06-04 13:58:55 +0200

    subheader entfernt

--- Streamlit/modules/migration_einbuergerung.py
+++ Streamlit/modules/migration_einbuergerung.py
@@ -12,18 +12,15 @@ def show():
         pass
     
     with tab2:
-        st.subheader("Ländergruppierungen")
         default_groups = ["Afrika", "Asien", "Europa", "Amerika"]
         simple_timeline("einbürg_ländergruppen.csv", "Ländergruppierungen", default_groups)
 
     with tab3:
-        st.subheader("Staatsangehörigkeit")
         default_groups = ['Türkei', 'Italien', 'Ukraine', 'Syrien', 'Afghanistan']
         simple_timeline("einbürg_staaten.csv", "Staatsangehörigkeit", default_groups)
         simple_piechart("einbürg_staaten.csv", "Staatsangehörigkeit", True)
 
     with tab4:
-        st.subheader("Rechtsgrundlagen")
         default_groups = None
         simple_timeline("einbürg_recht.csv", "Rechtsgrundlagen", default_groups)
 
