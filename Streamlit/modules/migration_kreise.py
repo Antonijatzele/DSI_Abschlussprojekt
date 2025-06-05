d5bc667d25e36ab91dde32fc27a286d3c05a26e8
Irrux <clemens.zaengel@web.de>
2025-06-05 11:07:41 +0200

    Korrelationkoeffizieent Kreise nach Ländern

--- Streamlit/modules/migration_kreise.py
+++ Streamlit/modules/migration_kreise.py
@@ -51,28 +51,25 @@ def show():
     with tab1:
         st.subheader('Kreise')
         geojson_str, colormap = lade_geodaten()
-        m = erstelle_karte(geojson_str)
+        #m = erstelle_karte(geojson_str)
         
         # Farbskala (Legende) zur Karte hinzufügen
-        colormap.add_to(m)
+        #colormap.add_to(m)
 
         # Karte anzeigen
-        map_data = st_folium(m, width=700, height=700)
+        #map_data = st_folium(m, width=700, height=700)
     
     with tab2:
 
         # Titel
         st.subheader("Bevölkerungsdichte vs. Ausländeranteil")
 
-        # Fester Pfad zur CSV-Datei
         csv_path = "Streamlit/data/migration/scatter_plot_anteil_dichte.csv"
-
-        # CSV einlesen
         df = pd.read_csv(csv_path)
 
         # Dropdown-Menü für Aufenthaltstitel
-        titel_options = ["insgesamt"] + sorted(df["Ausgewählte Aufenthaltstitel"].dropna().unique().tolist())
-        selected_titel = st.selectbox("Wähle einen Aufenthaltstitel", titel_options, index=0)
+        titel_options = ["insgesamt"] + sorted(df["Staatsangehörigkeit"].dropna().unique().tolist())
+        selected_titel = st.selectbox("Staatsangehörigkeit", titel_options, index=0)
 
         # Daten filtern oder aggregieren
         if selected_titel == "insgesamt":
@@ -84,25 +81,44 @@ def show():
                 })
             )
         else:
-            df_filtered = df[df["Ausgewählte Aufenthaltstitel"] == selected_titel]
+            df_filtered = df[df["Staatsangehörigkeit"] == selected_titel]
 
         # Erstellen des Plotly Scatterplots
         fig = px.scatter(
             df_filtered,
             x="Dichte",
             y="Anteil",
-            color="KREISE",   # Für die Farbdifferenzierung nach Kreis
             title=f"Ausländeranteil ({selected_titel})",
             labels={
                 "Dichte": "Bevölkerungsdichte (Einwohner pro km²)",
                 "Anteil": "Ausländeranteil (%)"
             },
             color_continuous_scale='Viridis',  # Farbpalette
-
         )
 
-        # Anzeige des Plots in Streamlit
+        fig.update_layout(
+            width=700,
+            height=700,
+        )
+
+        st.plotly_chart(fig, use_container_width=False)
+
+
+        # Korrealtionsdisgram
+        df_corr = pd.read_csv("Streamlit/data/migration/scatter_plot_anteil_dichte_corr.csv")
+        fig = px.bar(
+            df_corr, 
+            x='Correlation', 
+            y='Staatsangehörigkeit', 
+            title='Korrelationen zwischen Bevölkerungsdichte und -anteil nach Ländern',
+            orientation='h'
+            )
+        fig.update_layout(
+            height=1200
+        )
         st.plotly_chart(fig)
+
+
 
     st.markdown("Quelle: [Destatis - Ausländerstatistik](https://www-genesis.destatis.de/datenbank/online/statistic/12521/details)")
 
