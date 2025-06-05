592f80df16e3180291d621117371a08259064c5c
Irrux <clemens.zaengel@web.de>
2025-06-05 14:37:55 +0200

    Laufende Summe eingefügt

--- Streamlit/modules/migration_migration.py
+++ Streamlit/modules/migration_migration.py
@@ -9,17 +9,17 @@ def show():
     with tab1:
         st.subheader("Migration ausländischer Staatbürger")
         default_groups = None
-        simple_timeline("wander_gesamt.csv", "Art", default_groups)
+        simple_timeline("wander_gesamt.csv", "Art", default_groups, running_sum=True)
 
     with tab2:
         st.subheader("Wanderungsaldo nach Herkunftsländern")
         default_groups = ['Türkei', 'Italien', 'Ukraine', 'Syrien', 'Afghanistan']
-        simple_timeline("wander_staaten.csv", "Herkunfts-/Zielländer", default_groups)
+        simple_timeline("wander_staaten.csv", "Herkunfts-/Zielländer", default_groups, running_sum=True)
 
     with tab3:
         st.subheader("Migration deutscher Staatbürger")
         default_groups = None
-        simple_timeline("wander_gesamt_de.csv", "Art", default_groups)
+        simple_timeline("wander_gesamt_de.csv", "Art", default_groups, running_sum=True)
 
     st.markdown("Quelle: [Destatis - Wanderungsstatistik](https://www-genesis.destatis.de/datenbank/online/statistic/12711/details)")
 
